d0a3bd3399dcdee4ced55f340ce20b49e9c583de
mooracle <yanuar.prakosa@gmail.com>
2024-12-25 20:32:27 +0700

    modify int test to make the output easier to read

--- utils/testfrm.c
+++ utils/testfrm.c
@@ -7,12 +7,13 @@
 
 void intAssertEquals(int value, int target, char* test_id)
 {
+    printf("\nTEST ID: %s\n", test_id);
     if (value == target) 
     {
-        printf("test pass: %s\n", test_id);
+        printf("test pass: the value %d is the same as target %d\n", value, target);
     } else
     {
-        printf("test failed: %s\n", test_id);
+        printf("test failed: \n" );
         printf("  cause: value %d is not the same as the target %d\n", value, target);
     }
 }
