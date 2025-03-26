54c1a10189e09772d39c4d809b4a03cf4bd4f3c1
mooracle <yanuar.prakosa@gmail.com>
2025-03-26 22:45:35 +0700

    header for Strassen's algorithm

--- chapter4/chap4.h
+++ chapter4/chap4.h
@@ -37,4 +37,34 @@ int** squareMatrixCross(int n, int A[n][n], int B[n][n]);
         int B[][]
     */
 
+int** basicStrassenCross(int x, int y, int z, int A[x][y], int B[y][z]);
+    /*  
+        FUNCTION: basicStrassenCross
+        matrix multiplication using Strassen's algorithm
+
+        Return: int** 2D array matrix result of multiplicaton
+        CAUTION: return pointer onto heap memory. Always clear afterwards to avoid memory leak!
+
+        Parameters: 
+        int x : number of rows in first matrix (A)
+        int y : number of column(s) of first matrix (A), rows in second matrix (B)
+        int z : number of column(s) of second matrix (B) 
+        int A[][] : first matrix with x rows and y columns
+        int B[][] : second matrix with y rows and z columns 
+    */
+
+int** squareStrassenCross(int n, int A[n][n], int B[n][n]);
+    /*  
+        FUNCTION: squareStrassenCross
+        matrix multiplication using Strassen's algorithm for square matrix both inputs
+
+        Return: int** 2D array matrix result of multiplicaton
+        CAUTION: return pointer onto heap memory. Always clear afterwards to avoid memory leak!
+
+        Parameters: 
+        int n : number of rows or columns for all matrices involved in calculation
+        int A[][]
+        int B[][]
+    */
+
 #endif
