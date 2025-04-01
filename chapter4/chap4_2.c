afa62cf0a3214df11a1e593bed0f31e7e85dda68
mooracle <yanuar.prakosa@gmail.com>
2025-04-01 19:54:24 +0700

    add sum matrix for strassen's algorithm

--- chapter4/chap4_2.c
+++ chapter4/chap4_2.c
@@ -60,3 +60,34 @@ int** squareMatrixCross(int n, int A[n][n], int B[n][n])
     
     return squareC;
 }
+
+int** sumStrassenMatrix(int size_Add, int row0A, int col0A, int** matrix_A, int row0B, int col0B, int** matrix_B)
+{
+    /*  
+        Add two (pointer based) matrices for Strassen's recursive steps
+         A[row0A to row0A + size_Add][col0A to col0A + size Add] + B[row0B to row0B + size_Add][col0B to col0B +_ size_Add]
+        Also correspond to different basis sizes 
+        WARNING: this is for strassen's algorithm use only
+        The function assumes that the size to add is ALWAYS a square matrix
+        It is the caller function responsibility to ensure that the size_Add is appropriate
+
+        RETURN: int** matrix with size = size_Add x size_Add
+
+        PARAMTERS:
+        int size_Add = the size of row or col of the addition area
+        int row0A = the starting point of row for matrix A to be used in addition operation
+        int col0A = the starting point of column for matrix A to be used in addtion operation
+        int** matrix_A = the matrix A 
+        int row0B = the starting point of row for matrix B to be used in addition operation
+        int col0B = the starting point of column for matrix B to be used in addtion operation
+        int** matrix_B = the matrix B
+    */
+    // initiate the result matrix
+    int** summedMatrix = initMatrix(size_Add, size_Add);
+    //start addition operations
+    for (int i = 0; i < size_Add; i++)
+        for (int j = 0; j < size_Add; j++)
+            summedMatrix[i][j] = matrix_A[row0A + i][col0A + j] + matrix_B[row0B + i][row0B + j];
+    
+    return summedMatrix;
+}
